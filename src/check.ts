37450b307a3581c1c9b26aea86179f5f3ed90ab4
Tony Do <tony.do@pison.com>
2022-06-14 16:48:07 -0500

    adding optional flag to reduce pr template overhead

--- src/check.ts
+++ src/check.ts
@@ -4,9 +4,11 @@ export = function(body: string) {
   if (body === null) {
     return { total: 0, remaining: 0 };
   }
-  
+
+  let regex: RegExp = new RegExp('\<\!\-\-(?:.|\n|\r)*?-->', 'g');
+
   let tokens: marked.TokensList = marked.lexer(body, { gfm: true });
-  console.log(tokens)
+
   let allTokens = tokens.flatMap(function mapper(token: marked.Token): any {
     let t = token as marked.Tokens.ListItem 
     let l = token as marked.Tokens.List
@@ -15,10 +17,12 @@ export = function(body: string) {
     }
     return l.items && l.items.length ? l.items.flatMap(mapper) : [l];
   });
-  
+
   let items: marked.Tokens.ListItem[] = allTokens.filter(token => token.type === 'list_item');
+  let optional = items.filter(item => item.text.match(regex));
+  console.log(optional.length)
   return {
-    total: items.filter(item => item.checked !== undefined).length,
-    remaining: items.filter(item => item.checked === false).length
+    total: items.filter(item => item.checked !== undefined).length - optional.length,
+    remaining: items.filter(item => item.checked === false).length - optional.length
   };
 };
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,14 +1,32 @@
 import  check from '../src/check';
 
 test("Test check for 1 uncompleted task", () => {
-  let md = `
+  let md: string = `
   Task to be completed
   - [x] task 1
   - [ ] task 2
   - [x] task 3
   `;
-
   let results = check(md)
   expect(results.total).toBe(3);
   expect(results.remaining).toBe(1);
 }); 
+
+test("Test check for no task found", () => {
+  let md: string = `No tasks here`;
+  let results = check(md)
+  expect(results.total).toBe(0)
+  expect(results.remaining).toBe(0)
+})
+
+test("Test for optional tasks", () => {
+  let md: string = `
+  Tasks to be completed with optional
+  - [x] task 1
+  - [ ] task 2 <!-- optional task -->
+  - [x] task 3
+  `;
+  let results = check(md)
+  expect(results.total).toBe(2)
+  expect(results.remaining).toBe(0)
+}) 
